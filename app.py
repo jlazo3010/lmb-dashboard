72f5920bac4012ce8f003d0bce436eb4265fefd7
jlazo3010 <jorge3010@ciencias.unam.mx>
2025-05-28 16:40:54 -0600

    Update app.py

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-##Base de usuarios
+'''##Base de usuarios
 AWS_ACCESS_KEY = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 BUCKET_NAME = os.environ['S3_BUCKET']
@@ -89,7 +89,7 @@ else:
 s3 = boto3.client('s3',
     aws_access_key_id=AWS_ACCESS_KEY,
     aws_secret_access_key=AWS_SECRET_KEY
-)
+)'''
 
 USU = 'Usuarios_mez.csv'
 
